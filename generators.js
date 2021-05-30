7474ed6629ca157636a549e256e4de5b15213740
Kevin da Silva <kevin.kds80@gmail.com>
2021-05-30 16:33:44 -0300

    Update generators.js

--- generators.js
+++ generators.js
@@ -24,6 +24,7 @@ const generateSequences = ({amount, min, max, alphabet}) => {
             if(pos < 0 || pos > alphabet.length-1) {
                 pos = 0;
             }
+            console.log(pos);
             sequence += alphabet[pos];
         }
         sequences.push(sequence);
